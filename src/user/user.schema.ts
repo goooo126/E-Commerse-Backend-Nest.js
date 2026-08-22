d00172b0b1a724864474fa1dd03242e7e7cb361a
George <gorgeadly126@gmail.com>
2026-08-22 19:48:59 +0300

    Integrates Mongoose for User entity management
    Defines a comprehensive user schema with detailed fields, validation rules, and timestamps to store full user profiles.
    
    Updates the user service to inject the Mongoose User model, enabling database interactions for creating and fetching user records. This replaces placeholder service logic with actual data persistence.

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -1,14 +1,70 @@
-
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import { timestamp } from 'rxjs';
 
 export type UserDocument = HydratedDocument<User>;
 
-@Schema()
+@Schema({ timestamps: true })
 export class User {
-  @Prop()
+  @Prop({
+    type: String,
+    required: true,
+    min: [3, 'Name Must at least 3 characters'],
+    max: [30, 'Name Must at least 30 characters'],
+  })
   name: string;
+  @Prop({ type: String, required: true, unique: true })
+  email: string;
+  @Prop({
+    type: String,
+    required: true,
+    min: [3, 'Password Must at least 3 characters'],
+    max: [20, 'Password Must at least 20 characters'],
+  })
+  password: string;
+  @Prop({
+    type: String,
+    required: true,
+    enum: ['user', 'admin'],
+  })
+  role: string;
 
+  @Prop({
+    type: String,
+  })
+  avatar: string;
+
+  @Prop({
+    type: Number,
+  })
+  age: number;
+
+  @Prop({
+    type: String,
+  })
+  phoneNumber: string;
+
+  @Prop({
+    type: String,
+  })
+  address: string;
+
+  @Prop({
+    type: Boolean,
+    enum: [false, true],
+  })
+  active: boolean;
+
+  @Prop({
+    type: String,
+  })
+  verificationCode: string;
+
+  @Prop({
+    type: String,
+    enum:['male','female']
+  })
+  gender: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
