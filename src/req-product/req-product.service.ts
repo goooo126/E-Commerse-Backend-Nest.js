40a33de8158b03a9a6ac2a290042167a86460ad9
George <gorgeadly126@gmail.com>
2026-09-09 12:48:11 +0300

    Refactor ReqProductService to enhance error handling and role-based access control in CRUD operations

--- src/req-product/req-product.service.ts
+++ src/req-product/req-product.service.ts
@@ -1,10 +1,16 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateReqProductDto } from './dto/create-req-product.dto';
 import { UpdateReqProductDto } from './dto/update-req-product.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { ReqProduct } from './req-product.schema';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { GetReqProductsDto } from './dto/get-reqProducts.dto';
+import { Role } from 'src/user/enums/roles.enum';
 
 @Injectable()
 export class ReqProductService {
@@ -28,6 +34,11 @@ export class ReqProductService {
       user: user.id,
     });
 
+    await newReqProduct.populate({
+      path: 'user',
+      select: 'name email',
+    });
+
     return {
       status: 201,
       message: 'The Request product created successfully',
@@ -39,7 +50,7 @@ export class ReqProductService {
     const { limit = 10, skip = 0 } = query;
 
     //* check the role Admin:
-    if (user.role === 'admin') {
+    if (user.role === Role.Admin) {
       const [reqProducts, total] = await Promise.all([
         this.reqProductModel.find().select('-__v').skip(skip).limit(limit),
         this.reqProductModel.countDocuments(),
@@ -61,7 +72,7 @@ export class ReqProductService {
     }
 
     //* check the role User:
-    if (user.role === 'user') {
+    if (user.role === Role.User) {
       const [reqProducts, total] = await Promise.all([
         this.reqProductModel
           .find({ user: user.id })
@@ -88,14 +99,106 @@ export class ReqProductService {
   }
 
   async findOne(id: string, user) {
-    return `This action returns a #${id} reqProduct`;
+    //* check if the id is a valid Id:
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('The Id is not valid');
+    }
+
+    //* check if the reqProduct is existed:
+    const existedReqProduct = await this.reqProductModel
+      .findById(id)
+      .select('-__v');
+    if (!existedReqProduct) {
+      throw new NotFoundException('The Request Product is not found');
+    }
+
+    //* Admin:
+    if (user.role === Role.Admin) {
+      await existedReqProduct.populate({
+        path: 'user',
+        select: 'name email',
+      });
+      return {
+        status: 200,
+        message: 'The Request Product founded successfully',
+        data: existedReqProduct,
+      };
+    }
+
+    //* User:
+    if ((user.role = Role.User)) {
+      //* check if this user own this requst product:
+      if (user.id !== existedReqProduct.user) {
+        throw new UnauthorizedException();
+      }
+      await existedReqProduct.populate({
+        path: 'user',
+        select: 'name email',
+      });
+
+      return {
+        status: 200,
+        message: 'The Request Product founded successfully',
+        data: existedReqProduct,
+      };
+    }
   }
 
-  update(id: string, updateReqProductDto: UpdateReqProductDto, user) {
-    return `This action updates a #${id} reqProduct`;
+  async update(id: string, updateReqProductDto: UpdateReqProductDto, user) {
+    //* check if the id is a valid Id:
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('The Id is not valid');
+    }
+
+    //* check if the reqProduct is existed:
+    const existedReqProduct = await this.reqProductModel
+      .findById(id)
+      .select('-__v');
+    if (!existedReqProduct) {
+      throw new NotFoundException('The Request Product is not found');
+    }
+
+    //* check if this user own this requst product:
+    if (user.id !== existedReqProduct.user) {
+      throw new UnauthorizedException();
+    }
+
+    const updatedReqProduct = await this.reqProductModel.findByIdAndUpdate(
+      id,
+      updateReqProductDto,
+      { new: true, fields: '-__v' },
+    );
+
+    await updatedReqProduct!.populate({
+      path: 'user',
+      select: 'name email',
+    });
+    return {
+      status: 200,
+      message: 'The Request Product updated successfully',
+      data: updatedReqProduct,
+    };
   }
 
-  remove(id: string, user) {
-    return `This action removes a #${id} reqProduct`;
+  async remove(id: string, user): Promise<void> {
+    //* check if the id is a valid Id:
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('The Id is not valid');
+    }
+
+    //* check if the reqProduct is existed:
+    const existedReqProduct = await this.reqProductModel
+      .findById(id)
+      .select('-__v');
+    if (!existedReqProduct) {
+      throw new NotFoundException('The Request Product is not found');
+    }
+
+    //* check if this user own this requst product:
+    if (user.id !== existedReqProduct.user) {
+      throw new UnauthorizedException();
+    }
+
+    await this.reqProductModel.findByIdAndDelete(id);
   }
 }
