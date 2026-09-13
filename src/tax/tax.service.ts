9e0e21ebc2e6e35167ba4cd62b340c3cbc359ca2
George <gorgeadly126@gmail.com>
2026-09-14 01:33:57 +0300

    Enhance TaxService with error handling for invalid IDs and non-existent Tax entries

--- src/tax/tax.service.ts
+++ src/tax/tax.service.ts
@@ -1,9 +1,13 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateTaxDto } from './dto/create-tax.dto';
 import { UpdateTaxDto } from './dto/update-tax.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Tax } from './tax.schema';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 
 @Injectable()
 export class TaxService {
@@ -24,19 +28,83 @@ export class TaxService {
     };
   }
 
-  findAll() {
-    return `This action returns all tax`;
+  async findAll() {
+    const tax = await this.taxModel.find().select('-__v');
+    return {
+      status: 200,
+      message: 'tax is fetched successfully',
+      data: tax,
+    };
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} tax`;
+  async findOne(id: string) {
+    //* check if the id is valid id:
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('The ID must be valid');
+    }
+
+    //* check if the Tax is existed:
+    const tax = await this.taxModel.findById(id).select('-__v');
+    if (!tax) {
+      throw new NotFoundException('The Tax is not found');
+    }
+
+    return {
+      status: 200,
+      message: 'The Tax is founded successfully',
+      data: tax,
+    };
   }
 
-  update(id: string, updateTaxDto: UpdateTaxDto) {
-    return `This action updates a #${id} tax`;
+  async update(id: string, updateTaxDto: UpdateTaxDto) {
+    //* check if the id is valid id:
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('The ID must be valid');
+    }
+
+    //* check if the Tax is existed:
+    const tax = await this.taxModel.findById(id).select('-__v');
+    if (!tax) {
+      throw new NotFoundException('The Tax is not found');
+    }
+
+    //* check the tax name unqiness:
+    if (updateTaxDto.name) {
+      const existedTax = await this.taxModel.findOne({
+        name: updateTaxDto.name,
+        _id: { $ne: id },
+      });
+
+      if (existedTax) {
+        throw new BadRequestException('The Tax is already existed');
+      }
+    }
+
+    //* update the Tax:
+    const updatedTax = await this.taxModel.findByIdAndUpdate(id, updateTaxDto, {
+      new: true,
+      fields: '-__v',
+    });
+    return {
+      status: 200,
+      message: 'The Tax is updated successfully',
+      data: updatedTax,
+    };
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} tax`;
+  async remove(id: string): Promise<void> {
+    //* check if the id is valid id:
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('The ID must be valid');
+    }
+
+    //* check if the Tax is existed:
+    const tax = await this.taxModel.findById(id).select('-__v');
+    if (!tax) {
+      throw new NotFoundException('The Tax is not found');
+    }
+
+    //* Delete Tax
+    await this.taxModel.findByIdAndDelete(id);
   }
 }
